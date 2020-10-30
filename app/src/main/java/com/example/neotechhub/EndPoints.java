fd9c2817e401dc23b27fce0cbf640409e1b921d1
Navneet2308 <Ns7444296@gmail.com>
2020-10-30 17:50:55 +0530

    GoogleSheet Registration Sucessfully

--- app/src/main/java/com/example/neotechhub/EndPoints.java
+++ app/src/main/java/com/example/neotechhub/EndPoints.java
@@ -2,5 +2,4 @@ package com.example.neotechhub;
 
 public class EndPoints {
     public static String registration_api="https://script.google.com/macros/s/AKfycbzdQo7OrGCEvT3FfVn4NEHws5fKn3Ucxq-XYsYfho_wgHIo-Sw/exec";
-    public static String showdata_api="https://script.google.com/macros/s/AKfycbzdQo7OrGCEvT3FfVn4NEHws5fKn3Ucxq-XYsYfho_wgHIo-Sw/exec";
 }
